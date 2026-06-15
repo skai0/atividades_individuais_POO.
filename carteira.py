bca3f8d6b18321fe41dff0252657cf541dd674e6
𝓼𝓴𝓪𝓲𝓸 <coffefun535@gmail.com>
2026-06-15 10:19:16 -0300

    Update carteira.py

--- carteira.py
+++ carteira.py
@@ -19,6 +19,15 @@ class Carteira():
         valor_convertido = self.converter_yuan(valor_yuan):
         return self.saldo - valor_convertido
 
+      carteira_usd = Carteira("USD", 10.0)
+      print("Soma de carteira USD + 50 yuan =", carteira_usd + 50.0)
+
+    carteira_brl = Carteira("BRL", 20.0)
+    print("Soma de carteira BRL + 50 yuan =", carteira_brl + 50.0)
+
+    carteira_cny = Carteira("CNY", 100.0)
+      print("Subtração de carteira CNY - 30 yuan =", carteira_cny - 30.0)
+
 
 
     
